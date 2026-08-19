0d0c2c6c0ebfae6b398b9f1d7acad32c04166ba4
Jeremy Burgos <jeremy.burgos@planvital.cl>
2026-08-19 14:49:52 -0400

    actualización locator title_cameras

--- src/test/java/locators/cameras_locators.java
+++ src/test/java/locators/cameras_locators.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public interface cameras_locators {
 
-    By title_cameras = By.xpath("//h2[text()='Cameras1']");
+    By title_cameras = By.xpath("//h2[text()='Cameras']");
     By div_added_cart = By.xpath("//div[contains(@class,'alert-success')]");
     By select_product_option = By.xpath("(//select[contains(@name,'option')])[1]");
     By button_addToCart = By.id("button-cart");
